5a9b40a5e9ddd186975de7418615bdf9a685a3e2
NEWCODINGWITHABHI <abhishekkumar01021995@gmail.com>
2023-03-12 03:18:53 +0530

    infinite-scrolling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+const API_URL="https://jsonplaceholder.typicode.com/photos"
+
 
 function App() {
+const [data,setData]=useState([]);
+const[pageData,setPageData]=useState([]);
+const[isScroll,setScroll]=useState(false);
+ 
+
+function addPhoto(){
+  console.log("add photo")
+  console.log(data,pageData);
+  // setPageData([...pageData,...data.slice(pageData.length,pageData.length+10)]);
+  // setPageData(prev=>[...prev,...prev]);
+  setScroll(true);
+}
+function scrollFun(){
+   
+   if(document.documentElement.scrollHeight-1<=window.innerHeight+window.scrollY){
+    console.log("condition matched")
+    addPhoto();
+   }
+}
+  useEffect(()=>{
+       fetch(API_URL).then(res=>res.json()).then(d=>{
+        setData(d);
+        setPageData(d.slice(0,10));
+       })
+       window.addEventListener("scroll",scrollFun);
+       return ()=>window.removeEventListener("scroll",scrollFun);
+  },[])
+
+  useEffect(()=>{
+    console.log(pageData.length,"length")
+      setPageData([...(data.slice(0,pageData.length+10))]);
+      setScroll(false);
+      console.log("ussefect2");
+  },[isScroll])
+  console.log(window.innerHeight);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='img-container'>
+        {
+          pageData.map((photo)=>{
+            return(
+            <div className='img-box' key={photo.id}>
+              <img src={photo.url} alt="" />
+            </div>
+
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
